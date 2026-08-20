6f076b69928083df220d472de447fa2eb461b8b0
Trace Myers <main@tracemyers.com>
2026-08-20 11:11:31 -0400

    removed some boilerplate from ComponentCreation

--- Source/TracesUnrealExtension/Public/ComponentCreation.h
+++ Source/TracesUnrealExtension/Public/ComponentCreation.h
@@ -7,12 +7,12 @@
 
 struct FCreateComponentParams
 {
+	USceneComponent* AttachTo = nullptr;
 	const wchar_t* CollisionProfile = L"";
 	bool bCastShadow      = false;
 	bool bVisible         = false;
 	bool bReceivesDecals  = false;
 	bool bSimulatePhysics = false;
-	USceneComponent* AttachTo = nullptr;
 	ECollisionEnabled::Type CollisionEnabled = ECollisionEnabled::NoCollision;
 	EComponentMobility::Type ComponentMobility = EComponentMobility::Movable;
 };
@@ -23,21 +23,8 @@ struct FCreateComponentParams
 		SetRootComponent(CreateDefaultSubobject<USceneComponent>(L"Root")); \
 	}
 
-#define CREATE_PRIMITIVE_COMPONENT_AS_ROOT(PropName, ...)													\
-	do {                                                                                                    \
-		PropName = CreateDefaultSubobject<std::remove_reference_t<decltype(*PropName)>>(TEXT(#PropName));   \
-		SetRootComponent(PropName);																			\
-		const FCreateComponentParams Params = {__VA_ARGS__};												\
-		if (*Params.CollisionProfile != L'\0') {															\
-			PropName->SetCollisionProfileName(Params.CollisionProfile);										\
-		}																									\
-		PropName->CastShadow = Params.bCastShadow;															\
-		PropName->SetVisibility(Params.bVisible);															\
-		PropName->bReceivesDecals = Params.bReceivesDecals;													\
-		PropName->SetSimulatePhysics(Params.bSimulatePhysics);												\
-		PropName->SetCollisionEnabled(Params.CollisionEnabled);												\
-		PropName->SetMobility(Params.ComponentMobility);													\
-	} while (0);
+// The size/functionality hierarchy from most to least is primitive component, scene component, actor component
+// I think the names are bad, but it's important to know so that you pick the right initialization macro.
 
 #define CREATE_PRIMITIVE_COMPONENT(PropName, ...) \
 	do {                                                                                                    \
@@ -55,14 +42,11 @@ struct FCreateComponentParams
 		PropName->SetMobility(Params.ComponentMobility);													\
 } while (0);
 
-#define CREATE_SCENE_COMPONENT_AS_ROOT(PropName, ...) \
-    do { \
-        PropName = CreateDefaultSubobject<std::remove_reference_t<decltype(*PropName)>>(TEXT(#PropName)); \
-        SetRootComponent(PropName); \
-        const FCreateComponentParams Params = {__VA_ARGS__}; \
-        PropName->SetVisibility(Params.bVisible); \
-        PropName->SetMobility(Params.ComponentMobility); \
-    } while (0);
+#define CREATE_PRIMITIVE_COMPONENT_AS_ROOT(PropName, ...)	\
+	do {													\
+		CREATE_PRIMITIVE_COMPONENT(PropName, __VA_ARGS__)	\
+		SetRootComponent(PropName);							\
+	} while (0);
 
 #define CREATE_SCENE_COMPONENT(PropName, ...)																\
 	do {                                                                                                    \
@@ -71,6 +55,12 @@ struct FCreateComponentParams
 		PropName->SetupAttachment(Params.AttachTo ? Params.AttachTo : GetRootComponent());					\
 		PropName->SetVisibility(Params.bVisible);															\
 	} while (0);
+
+#define CREATE_SCENE_COMPONENT_AS_ROOT(PropName, ...)	\
+    do {												\
+		CREATE_SCENE_COMPONENT(PropName, __VA_ARGS__)	\
+        SetRootComponent(PropName);						\
+    } while (0);
 
 #define CREATE_ACTOR_COMPONENT(PropName)																	\
 	do {                                                                                                    \
